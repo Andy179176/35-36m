dbc5457e5708defcb8a147601e53d679b7e63f53
andy179176 <andy179176@gmail.com>
2022-08-29 14:25:50 +0200

    20220829

---
+++ j20220829_2/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int counter=5;
+        while (counter>0){
+            System.out.printf("  count: ( %d )%n",counter);
+            counter=counter-1;
+        }
+        System.out.println("counter after while "+ counter);
+        System.out.println( "--- start ----");
+
+    }
+}
---
+++ j20220829_3/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=10;
+        int b=50;
+
+        while (a<=b){
+            if ( a%2==0 ) {
+                System.out.println(a);
+            }
+            a=a+1;
+        }
+
+    }
+}
